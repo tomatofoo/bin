8d8a1f030dc029e132d17c3e221146ad583fc902
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-06-21 02:44:40 -0700

    make randomize_mac a proper shell script

--- randomize_mac.sh
+++ randomize_mac.sh
@@ -1,6 +1,42 @@
 #!/usr/bin/env bash
 
-sudo ifconfig wlp3s0 down
+
+NAME=$(basename $0)
+PREHELP="Try $NAME -h for more information."
+HELP="Usage: $NAME [OPTIONS]
+
+options:
+    -h             show this help
+    -d <device>    set which device to use (default wlp3s0)
+
+"
+
+opt_error() {
+    echo "Error: invalid option: -$1"
+}
+
+
+DEVICE="wlp3s0"
+while getopts ":hd:" opt; do
+    case "$opt" in 
+        "h")
+            echo "$HELP"
+            exit
+            ;;
+        "d")
+            DEVICE="$OPTARG"
+            ;;
+        *)
+            opt_error "$OPTARG"
+            echo "$PREHELP"
+            exit
+            ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+echo "Randomizing MAC address on $DEVICE..."
+sudo ifconfig "$DEVICE" down
 sudo macchanger -r wlp3s0
-sudo ifconfig wlp3s0 up
+sudo ifconfig "$DEVICE" up
 
